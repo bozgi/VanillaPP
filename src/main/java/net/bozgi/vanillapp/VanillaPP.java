520a91ad489139dcd884ac2fd886f42cacf934ea
oskar <oskaros040@gmail.com>
2023-01-17 16:45:48 +0100

    Initial commit

---
+++ src/main/java/net/bozgi/vanillapp/VanillaPP.java
@@ -0,0 +1,15 @@
+package net.bozgi.vanillapp;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VanillaPP implements ModInitializer {
+	public static final String MOD_ID = "vanillapp";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bozgi/vanillapp/VanillaPPModClient.java
@@ -0,0 +1,11 @@
+package net.bozgi.vanillapp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaPPModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
